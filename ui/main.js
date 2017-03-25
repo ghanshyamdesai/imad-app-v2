449e2d304ac7ccf7e1cd7c37b3322c6bfbb0380f
ghanshyamdesai <desaighanshyam93@gmail.com>
2017-03-25 15:25:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,21 @@
-var button=document.getElementById('counter');
+var button = document.getElementById('counter');
 
-button.onclick=function(){
+button.onclick = function () {
     
-    var request=new XMLHttpRequest();
-    request.onreadystatechange = function(){
-     if(request.readystate== XMLHttpRequest.DONE)
-     {
-         if(request.status==200)
-         {
-             var counter=request.responseText;
-             var span=document.getElementById('span');
-             span.innerHTML=counter.toString();
-             
-         }
-     }
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and show it in a variable
+    request.onreadystatechange = function () {
+       if(request.readyState === XMLHttpRequest.DONE){
+           if(request.status === 200){
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+           }
+       }
     };
-    request.open('GET','http://ghanshyamdesai.imad.hasura-app.io/counter',true);
+    //making the rquest declaration
+    request.open('GET',"http://paraskumarsahu.imad.hasura-app.io/counter",true);
     request.send(null);
-    
 };
